86477c6f33c4cb5f358535a4f2b793ff7c784403
mkchauhan647 <mkchauhan647@gmail.com>
2024-05-27 12:22:39 +0545

    initial setup

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ const handle = nextApp.getRequestHandler();
 const server = express();
 
 
-
 nextApp.prepare().then(() => {
 
     server.set('view engine', 'ejs');
@@ -30,6 +29,10 @@ nextApp.prepare().then(() => {
 
     });
 
+         // Example of an API route
+    server.get('/api/hello', (req, res) => {
+        res.json({ message: 'Hello from Express!' });
+    });
     // Define your API routes or any other server logic here
 
     server.all('*', (req, res) => {
