21f586aa8c803a96710476b0c41f9a4993c6a8f4
mkchauhan647 <mkchauhan647@gmail.com>
2024-05-27 13:03:14 +0545

    i

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-// export default nextConfig;
 
-module.exports = {
-    async rewrites() {
-        return [
-            {
-                source: '/api/*',
-                destination: '/server/server.js',
-            },
-        ];
-    },
-};
+export default nextConfig;
